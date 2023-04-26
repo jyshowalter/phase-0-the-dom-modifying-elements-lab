9330d353f1cc179d46543c39eebfeece8cf139dc
Jason <jyshowalter@gmail.com>
2023-04-25 19:14:07 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.appendChild()
+document.body.appendChild(newHeader)
+newHeader.id = "Victory";
+newHeader.textContent = "Jason is the champion";
